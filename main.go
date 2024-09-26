5d6fa640253501042d1ad6dbae633a4e95900559
--replace-all <amanssinha9@gmail.com>
2024-09-26 15:26:48 -0400

    reversing the changes back to start

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
+
 import "fmt"
 
-func max(a int, b int) int {
-    if a > b {
-        return a
-    }
-    return b
-}
-
 func main() {
-    maxNum := max(10, 20)
-    fmt.Println("Maximum is:", maxNum)
+	fmt.Println("Welcome to Group C's Week 4 Project!")
 }
