0efcae41c6357509e2c2c479b18f2ff4a569c74b
preethamsgithub <144062645+preethamsgithub@users.noreply.github.com>
2024-09-26 17:13:14 -0400

    Create max.go

---
+++ max.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// func main() {
+// 	maxNum := max(10, 20)
+// 	fmt.Println("Maximum is:", maxNum)
+// }
